96b9e754e4f76f330b2c9c1456d580e6290a7e85
Alexandra Fren <alexandra.fren@gmail.com>
2018-06-11 15:38:27 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -17,7 +17,8 @@ end
 end
 
 def swap_elements(array)
-  array[1] = array[2]
-  array[2] = array[1]
-  return array
+  new_array = array
+  new_array[1] = array[2]
+  new_array[2] = array[1]
+  return newarray
 end
