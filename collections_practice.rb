53a12f44908d0b4fc5b6342f56017c20d5ad2cd7
Alexandra Fren <alexandra.fren@gmail.com>
2018-06-11 15:24:44 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,12 +1,11 @@
 def sort_array_asc(array)
   array.sort do |a,b|
-    a <=> b 
-  end 
+    a <=> b
+  end
 end
 
 def sort_array_desc(array)
   array.sort do |a,b|
-    b <=> a 
-  end 
+    b <=> a
+  end
 end
-
